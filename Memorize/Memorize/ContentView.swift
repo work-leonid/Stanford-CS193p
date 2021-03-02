f61769d523ca5956c6fe7e853b804a1419739f9a
leonid nazarov <work.leonid@gmail.com>
2021-03-02 09:29:00 +0300

    add code from first lection

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(.largeTitle)
+    }
+}
+
+struct CardView: View {
+    
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 3)
+                Text("👻")
+            }
+            else {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill()
+            }
+        }
     }
 }
 
